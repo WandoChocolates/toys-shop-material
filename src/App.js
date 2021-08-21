1048a5088e224d1bfbacde24f9dde6327338a93a
WandoChocolates <ellytitus50@gmail.com>
2021-08-21 20:38:09 +0800

    Commit day

--- src/App.js
+++ src/App.js
@@ -1,43 +1,26 @@
 import React from "react";
 import "./style.css";
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import { loadCSS } from "fg-loadcss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./components/Header";
 
 export default function App() {
-  const classes = useStyles();
+  React.useEffect(() => {
+    const node = loadCSS(
+      "https://use.fontawesome.com/releases/v5.12.0/css/all.css",
+      document.querySelector("#font-awesome-css")
+    );
+
+    return () => {
+      node.parentNode.removeChild(node);
+    };
+  }, []);
 
   return (
     <React.Fragment>
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-      </AppBar>
-    </div>
+      <Router>
+        <Header />
+      </Router>
     </React.Fragment>
   );
 }
